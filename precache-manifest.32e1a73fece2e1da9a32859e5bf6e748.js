ed491f2beacaad3e1c5ba636f6a3e983ab12f2d2
bc464 <fil419@webmail.co.za>
2022-09-27 17:50:50 +0200

    Updates

---
+++ precache-manifest.32e1a73fece2e1da9a32859e5bf6e748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ecc0ab6e2aee36b64953a035748c3d",
+    "url": "/tours-react/index.html"
+  },
+  {
+    "revision": "680244690590b5e78fbb",
+    "url": "/tours-react/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours-react/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-react/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680244690590b5e78fbb",
+    "url": "/tours-react/static/js/main.aa5ae904.chunk.js"
+  },
+  {
+    "revision": "dc60b3b845f19aebdc7c",
+    "url": "/tours-react/static/js/runtime-main.27026fda.js"
+  }
+]);
